c836c7b8887a5c339834b4604be7f34bc8f3cdf6
NicholasSaltis <gcas012107@coderacademy.edu.au>
2021-05-20 17:27:40 +1000

    add convert by flour weight method for implementation into class. not fully tested.

--- src/modules/converters.rb
+++ src/modules/converters.rb
@@ -4,3 +4,16 @@ require 'json'
 require 'tty-prompt'
 Bundler.require(:default)
 $prompt = TTY::Prompt.new
+
+
+
+def convert_by_flour_weight(ingredients)
+  input = gets.chomp.to_i
+  flour = ingredients[0]
+  total = input / flour[:flour]
+  ingredients.each do | ingredient |
+    ingredient.each do | i,p |
+      puts "#{i}: #{(total*p).to_i}grams"
+    end
+  end
+end
