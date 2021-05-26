de87252279f3bb72c9f83d579dbbe4a5215cfffe
NicholasSaltis <gcas012107@coderacademy.edu.au>
2021-05-26 13:54:29 +1000

    added logic for rejecting user input if it is not integer and retrying input until correct. modularised into sep method to be called by both conversion methods

--- src/modules/converters.rb
+++ src/modules/converters.rb
@@ -7,16 +7,32 @@ require_relative "./RecipeBook.rb"
 Bundler.require(:default)
 $prompt = TTY::Prompt.new
 
-
+def get_user_integer
+  valid_input = false
+  input = 0
+  until valid_input do
+    user_input = gets.chomp.to_i
+    if user_input > 0 && user_input.kind_of?(Integer)
+      valid_input = true
+      input += user_input
+      clear
+      break
+    else
+      puts "Incorrect input, please enter an integer greater than 0"
+    end
+  end
+  return input
+end
 # conversion method to be called by conversion_options passing a recipe hash as an argument. 
 # takes user input integer and divides it by the flour value in the recipe hash to find total weight of recipe
 # iterates over hash first printing recipe name, then ingredient name and quantity in grams by multiplying total weight by the float value provided by hash.
 def convert_by_flour_weight(ingredients)
   puts "Please input a number for the amount of grams of flour to be used"
-  input = gets.chomp.to_i
+  input = get_user_integer
   total_weight = 0
   flour = ingredients[0]
   total = input / flour["flour"]
+  ascii_heading("Conversions")
   ingredients.each do | ingredient |
     ingredient.each do | i,p |
       puts "#{i}: #{(total*p).to_i}grams"
@@ -30,7 +46,8 @@ end
 # takes user input integer and iterates over recipe hash multiplying each value by the total weight and printing the result.
 def convert_by_total_weight(ingredients)
   puts "Please input a number for the total target weight of this recipe"
-  input = gets.chomp.to_i
+  input = get_user_integer
+  ascii_heading("Conversions")
   ingredients.each do | ingredient |
     ingredient.each do | i,p |
         puts "#{i}: #{(input*p).to_i} grams"
